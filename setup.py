20eb99282bd318f81b9f9cf5775dc2fdd141ef0b
Asif Ahmed <public.quickgrid@gmail.com>
2023-04-15 08:08:18 +0600

    fix path

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vq_compress',
+    author='Asif Ahmed',
+    description='Image compression with vqgan, autoencoder etc.',
+    version='0.0.1',
+    url='https://github.com/quickgrid/vq-compress',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+        'numpy',
+        'omegaconf',
+        'safetensors',
+        'torch',
+        'torchvision',
+        'pillow',
+        'tqdm'
+    ]
+)
