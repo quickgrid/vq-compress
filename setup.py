c87a8b9a1e84935009907e87d792aa311b71750e
Asif <public.quickgrid@gmail.com>
2023-04-15 19:26:15 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='vq_compress',
     author='Asif Ahmed',
     description='Image compression with vqgan, autoencoder etc.',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/quickgrid/vq-compress',
     packages=find_packages(),
     classifiers=[
